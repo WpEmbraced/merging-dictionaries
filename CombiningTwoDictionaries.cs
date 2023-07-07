a255f0a5961462a48e7abae56d310e243cee00af
WpEmbraced <45814109+WpEmbraced@users.noreply.github.com>
2023-07-07 16:14:09 -0400

    Update CombiningTwoDictionaries.cs

--- CombiningTwoDictionaries.cs
+++ CombiningTwoDictionaries.cs
@@ -1,14 +1,14 @@
 Dictionary<string, string> dict1 = new Dictionary<string, string> { { "Superman", "Flight" } };
 Dictionary<string, string> dict2 = new Dictionary<string, string> { { "Batman", "Gadgets" } };
 
-// Using LINQ
+// Using LINQ Concat method and ToDictionary
 var merged = dict1.Concat(dict2).ToDictionary(x => x.Key, x => x.Value);
 
-// Using a foreach loop
+// Using foreach loop to merge dictionaries
 foreach (var item in dict2)
 {
     dict1[item.Key] = item.Value;
 }
 
-// Using the Union extension method
+// Using the Union extension method and ToDictionary
 var merged2 = dict1.Union(dict2).ToDictionary(x => x.Key, x => x.Value);
